f2935e5989f8b4a5f34770973e5c409a68c00bf4
Zhou Yaoyang <diamondzyy@sina.com>
2018-01-11 15:02:54 +0800

    finished C++ version

--- dep_count.cc
+++ dep_count.cc
@@ -1,8 +1,11 @@
 //
 // Created by zyy on 1/11/18.
 //
-#include <unordered_map>
+#include <map>
+#include <array>
 #include <iostream>
+#include <cinttypes>
+#include <ctime>
 
 
 #include "protoio.hh"
@@ -10,6 +13,8 @@
 
 
 using namespace std;
+
+array<uint32_t, 400'000'000> counts;
 
 
 void countDependancy(ProtoInputStream &trace) {
@@ -20,18 +25,34 @@ void countDependancy(ProtoInputStream &trace) {
     cout << "skip header!\n";
 
     uint64_t num_msg = 0;
+    clock_t start_s = clock();
     while (trace.read(inst)) {
         num_msg += 1;
         if (inst.reg_dep_size()) {
             for (auto &it: inst.reg_dep()) {
-                cout << it;
+                counts[it] += 1;
             }
-            cout << "\n";
         }
-        if (num_msg > 100) {
-            break;
+        if (num_msg % 10'000'000 == 0) {
+            cout << "*";
         }
     }
+    clock_t end_s = clock();
+    cout << "It takes " << (end_s - start_s)/(double(CLOCKS_PER_SEC))
+        << "s to get count array of " << num_msg << " insts\n";
+
+    start_s = clock();;
+    map<uint32_t, uint32_t> dep_map;
+    for (auto &it: counts) {
+        if (!dep_map.count(it)) {
+            dep_map[it] = 1;
+        } else {
+            dep_map[it] += 1;
+        }
+    }
+    end_s = clock();
+    cout << "It takes " << (end_s - start_s)/(double(CLOCKS_PER_SEC))
+        << "s to get dep map\n";
 }
 
 
